a5e7ab08f9045259d926acdf9d866ec225f6afcc
myosumonaung.github.io <myosumonaung91@gmail.com>
2025-11-27 19:59:09 +0900

    intro php

---
+++ ex.php
@@ -0,0 +1,9 @@
+<!-- if/else: write a script that checks if a number is even or odd. -->
+<?php
+    $number = 5;
+    if($number %2 == 0){
+        echo "$number is even.";
+    }else{
+        echo "$number is odd.";
+    }
+?>
--- index.php
+++ index.php
@@ -1,3 +1,62 @@
 <?php
-    echo "Hello world!"
+    echo "Hello world!";
+    echo "<br>";
+    $name = "John";
+    echo "Hello $name";
+    // string, integer, float, boolean, array, object, null
+    $age = 25;
+    echo "<br>";
+    echo "Hello $name, you are $age years old.";
+
+    $height = 1.80;
+    echo "<br>";
+    echo "Hello $name, you are $age years old and $height meters tall.";
+
+    // $is_married = false;
+    $is_married = true;
+    echo "<br>";
+    echo "Hello $name, you are $age years old and $height meters tall and $is_married.";
+
+    $hobbies = array("coding","sleeping","eating");
+    echo "<br>";
+    echo "Hello $name, you are $age years old and $height meters tall and $is_married, Your hobbies are $hobbies[0], $hobbies[1], $hobbies[2].";
+
+    // control flow 
+    // if, else,elseif, switch 
+    if($age < 18){
+        echo "<br>";
+        echo "You are not allowed to vote.";
+    }else{
+        echo "<br>";
+        echo "You are allowed to vote.";
+    }
+
+    // loops 
+    // while, do while, for 
+    while ($age < 18) {
+        echo "<br>";
+        echo "You are not allowed to vote.";
+        $age++;
+    }
+
+    for ($i=0; $i < 5; $i++) { 
+        echo "<br>";
+        echo "Hello $name";
+    }
+
+    // functions 
+    // function name($parameter){
+    //     ... code
+    // }
+    function sayHello($name){
+        echo "<br>";
+        echo "Hello $name";
+    }
+    sayHello("John");
 ?>
+
+<!-- php statements -->
+<!-- variable and types -->
+<!-- control flow -->
+<!-- loops  -->
+ <!-- functions  -->
